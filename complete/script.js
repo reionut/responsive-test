a2745d32de33b3f3ce4be622ccc927b5578acb1b
reionut <39769679+reionut@users.noreply.github.com>
2018-07-15 09:44:57 +0300

    Implemented rotating button

--- complete/script.js
+++ complete/script.js
@@ -6,7 +6,9 @@ document.querySelector('#aboutButton').addEventListener('click', function(){
   hideAboutUs = !hideAboutUs;
   if (hideAboutUs === true) {
     document.querySelector('#aboutContainer').className = 'container hide';
+    aboutImgRotate.style.transform = 'rotate(180deg)';
   } else {
     document.querySelector('#aboutContainer').className = 'container';
+    aboutImgRotate.style.transform = 'rotate(360deg)';
   }
 });
